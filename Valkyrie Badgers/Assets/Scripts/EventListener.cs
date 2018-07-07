9bbfcae75fd2f9c1afc8fd5b623ffa8cf76bf03e
Tobias Kruseborn <tobias.kruseborn@raysearchlabs.com>
2018-07-07 21:52:14 +0200

    Load prev or next room when clicking on sides of screen

---
+++ Valkyrie Badgers/Assets/Scripts/EventListener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventListener : MonoBehaviour {
+
+  void Update () {
+		if (Input.GetMouseButtonDown(0))
+    {
+      float MousePosx = Input.mousePosition.x;
+      int Scene = SceneManager.GetActiveScene().buildIndex;
+      if ( MousePosx < 100 && Scene > 0 )
+      {
+        SceneManager.LoadScene(Scene - 1);
+      }
+      else if ( MousePosx > Screen.width - 100 && Scene + 1 < SceneManager.sceneCount )
+      {
+        SceneManager.LoadScene(Scene + 1);
+      }
+    }
+	}
+}
