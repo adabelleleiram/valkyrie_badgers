e94e5c7d5bcd6536847193584478adab3bdc6127
Adabelle Leiram <adabelle.leiram@gmail.com>
2018-07-08 18:53:11 +0200

    Mouse Cursor

---
+++ Valkyrie Badgers/Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour {
+
+    public GameObject defaultCursor;
+
+    GameObject currentCursor;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        SetCursor(defaultCursor);
+    }
+
+    public void SetCursor(GameObject cursorObject)
+    {
+        if (currentCursor)
+            currentCursor.SetActive(false);
+
+        currentCursor = cursorObject;
+        currentCursor.SetActive(true);
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
