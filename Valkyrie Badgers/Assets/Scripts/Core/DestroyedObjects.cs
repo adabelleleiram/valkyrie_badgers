cd0dede86cc4030dbe012284b8596c94428e84d0
Tobias Kruseborn <tobias.kruseborn@raysearchlabs.com>
2018-07-10 06:26:01 +0200

    Add destroyed objects code

---
+++ Valkyrie Badgers/Assets/Scripts/Core/DestroyedObjects.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DestroyedObjects
+{
+  static List<GameObject> objs = new List<GameObject>();
+  public static void Add(GameObject obj)
+  {
+     objs.Add(obj);
+  }
+}
