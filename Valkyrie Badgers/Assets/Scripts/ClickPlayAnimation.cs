8940c97bad1b94d23ac69ee7c4c8caf253942d2e
Bobbi Augustine Sand <bobbi@ozma.se>
2018-07-10 23:33:25 +0200

    Added animation files + script and wisp graphics to test with

---
+++ Valkyrie Badgers/Assets/Scripts/ClickPlayAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickPlayAnimation : MonoBehaviour 
+
+{
+		
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{	
+				
+			// play the attached sound
+			//GetComponent<AudioSource>().Play();
+
+			//trigger animation
+                        {
+                            Animator anim = GetComponent<Animator>();
+                            anim.SetTrigger("Flying_Room1");
+                        
+                        }
+
+		}
+	}
+}
