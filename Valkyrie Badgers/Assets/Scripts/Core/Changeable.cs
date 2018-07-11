c9c9112b9c2c361cf4fa963c6200aa577adef7dc
Tobias Kruseborn <tobias.kruseborn@raysearchlabs.com>
2018-07-11 09:24:38 +0200

    Enable clicking on object to reveal and unlock other objects

--- Valkyrie Badgers/Assets/Scripts/Core/Changeable.cs
+++ Valkyrie Badgers/Assets/Scripts/Core/Changeable.cs
@@ -11,19 +11,40 @@ public class DataChangable
     scene = SceneManager.GetActiveScene().buildIndex;
   }
 
-  public string name = "";
+  public string name;
   public bool activated = false;
-  public int scene = 0;
+  public int scene = -1;
+}
+
+public class Unlocked
+{
+  public Unlocked(string nameIn)
+  {
+    name = nameIn;
+    scene = SceneManager.GetActiveScene().buildIndex;
+  }
+
+  public string name;
+  public int scene = -1;
 }
 
 public class Changeable : MonoBehaviour
 {
-  public List<DataChangable> list = new List<DataChangable>();
+  List<DataChangable> list = new List<DataChangable>();
+  List<Unlocked> unlockedItems = new List<Unlocked>();
+  public int numUnlocked = 0;
 
   public void ChangeActive(GameObject obj, bool active)
   {
     DataChangable data = new DataChangable(obj.name, active);
     list.Add(data);
+  }
+
+  public void Unlock(GameObject obj)
+  {
+    Unlocked item = new Unlocked(obj.name);
+    unlockedItems.Add(item);
+    numUnlocked++;
   }
 
   public void UpdateState(GameObject obj)
@@ -34,6 +55,18 @@ public class Changeable : MonoBehaviour
       if (data.name == obj.name && data.scene == scene )
       {
         obj.SetActive(data.activated);
+      }
+    }  
+  }
+
+  public void UpdateUnlocked(LockedObj obj)
+  {
+    int scene = SceneManager.GetActiveScene().buildIndex;
+    foreach (Unlocked unlocked in unlockedItems)
+    {
+      if (unlocked.name == obj.name && unlocked.scene == scene)
+      {
+        obj.locked = false;
       }
     }
   }
