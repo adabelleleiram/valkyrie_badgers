04dbbb2e8b949b519cd60a0e98866829e2eff0b0
Adabelle Leiram <adabelle.leiram@gmail.com>
2018-07-11 20:05:34 +0200

    OnClick

---
+++ Valkyrie Badgers/Assets/Scenes/Scripts/OnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnClick : MonoBehaviour {
+
+    [System.Serializable]
+    public class ClickEvent : UnityEvent { }
+
+    public ClickEvent onClick;
+
+    private void OnMouseUpAsButton()
+    {
+        onClick.Invoke();
+    }
+}
