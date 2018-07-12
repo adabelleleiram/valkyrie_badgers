6accc4d535121f0d61766ebff39559cda9376a82
Tobias Kruseborn <tobias.kruseborn@raysearchlabs.com>
2018-07-12 20:31:40 +0200

    Remove wisp on destory if clicked

---
+++ Valkyrie Badgers/Assets/Scripts/OnChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnChangeScene : MonoBehaviour
+{
+  [System.Serializable]
+  public class DestroyEvent : UnityEvent { }
+
+  public DestroyEvent onDestroy;
+  bool clicked = false;
+
+  private void OnDestroy()
+  {
+    if (clicked)
+    {
+      onDestroy.Invoke();
+      GameHandler.persistencyManager.SetActiveState(gameObject);
+    }
+  }
+
+  public void SetClicked()
+  {
+    clicked = true;
+  }
+}
