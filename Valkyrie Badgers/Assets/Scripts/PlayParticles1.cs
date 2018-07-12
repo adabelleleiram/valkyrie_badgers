7a08d505d809c02a2a08d274764c4e1cd6faae36
Bobbi Augustine Sand <bobbi@ozma.se>
2018-07-12 21:22:45 +0200

    Added particle system script

---
+++ Valkyrie Badgers/Assets/Scripts/PlayParticles1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayParticles1 : MonoBehaviour {
+
+    [SerializeField]
+    private ParticleSystem particleObject;
+
+    void Start()
+    {
+        particleObject = GetComponent<ParticleSystem>();
+        particleObject.Stop();
+    }
+    public void Play () 
+    {
+        Debug.Log("Play:");
+        particleObject.Play();
+    }
+
+}
