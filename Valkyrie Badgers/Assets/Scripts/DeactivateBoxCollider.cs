22cb9a2db9e491003c668353e5506250b481952b
Tobias Kruseborn <tobias.kruseborn@raysearchlabs.com>
2018-07-13 13:06:33 +0200

    Changes

---
+++ Valkyrie Badgers/Assets/Scripts/DeactivateBoxCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateBoxCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    gameObject.GetComponent<Collider>().enabled = false;
+  }
+
+  public void Revert()
+  {
+    gameObject.GetComponent<Collider>().enabled = true;
+  }
+}
