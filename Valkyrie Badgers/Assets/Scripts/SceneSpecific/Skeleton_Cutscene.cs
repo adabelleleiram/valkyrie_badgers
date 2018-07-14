9dbb171482785539888c63165b25707a1f8086e8
Adabelle Leiram <adabelle.leiram@gmail.com>
2018-07-14 13:50:46 +0200

    Feather puzzle

--- Valkyrie Badgers/Assets/Scripts/SceneSpecific/Skeleton_Cutscene.cs
+++ Valkyrie Badgers/Assets/Scripts/SceneSpecific/Skeleton_Cutscene.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Skeleton_Cutscene : MonoBehaviour
 {
     public LoopSequenceTrigger musicTrigger;
+    public SceneField finalScene;
+
+    public List<GameObject> wings = new List<GameObject>();
+
+    public float minFeatherFadeInTime;
+    public float wingShowTime;
+    public float fadeToWhiteTime;
     
 	public void TriggerCutscene()
     {
