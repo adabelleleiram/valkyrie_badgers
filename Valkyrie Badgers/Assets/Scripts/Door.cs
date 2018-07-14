c9c9ec760fbad093a4106b92b41b8ad4d6947e75
Adabelle Leiram <adabelle.leiram@gmail.com>
2018-07-14 18:53:57 +0200

    onunlock

--- Valkyrie Badgers/Assets/Scripts/Door.cs
+++ Valkyrie Badgers/Assets/Scripts/Door.cs
@@ -1,58 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-  public SceneField nextScene;
-  public bool locked = false;
-  public bool left = false;
+    public SceneField nextScene;
+    public bool locked = false;
+    public bool left = false;
 
-  private void OnMouseUpAsButton()
-  {
-    if (!locked)
-      GameHandler.sceneLoader.LoadScene(nextScene);
-  }
-  private void OnMouseEnter()
-  {
-    if (!locked)
+    [System.Serializable]
+    public class UnlockEvent : UnityEvent { }
+
+    public UnlockEvent onUnlock;
+
+    private void OnMouseUpAsButton()
     {
-      if (left)
-        MouseCursor.instance.SetCursor(MouseCursor.instance.leftDoorCursor);
-      else
-        MouseCursor.instance.SetCursor(MouseCursor.instance.rightDoorCursor);
+        if (!locked)
+            GameHandler.sceneLoader.LoadScene(nextScene);
     }
-  }
+    private void OnMouseEnter()
+    {
+        if (!locked)
+        {
+            if (left)
+                MouseCursor.instance.SetCursor(MouseCursor.instance.leftDoorCursor);
+            else
+                MouseCursor.instance.SetCursor(MouseCursor.instance.rightDoorCursor);
+        }
+    }
 
-  void OnMouseExit()
-  {
-    MouseCursor.instance.SetCursor(MouseCursor.instance.defaultCursor);
-  }
+    void OnMouseExit()
+    {
+        MouseCursor.instance.SetCursor(MouseCursor.instance.defaultCursor);
+    }
 
-  public void Unlock()
-  {
-    locked = false;
-  }
+    public void Unlock()
+    {
+        locked = false;
+        onUnlock.Invoke();
+    }
 
-  private void Awake()
-  {
-    GameHandler.sceneLoader.onNewSceneLoading += OnNewSceneLoading;
-  }
+    private void Awake()
+    {
+        GameHandler.sceneLoader.onNewSceneLoading += OnNewSceneLoading;
+    }
 
-  private void Start()
-  {
-    locked = GameHandler.persistencyManager.GetLockedState(gameObject, locked);
-  }
+    private void Start()
+    {
+        locked = GameHandler.persistencyManager.GetLockedState(gameObject, locked);
+    }
 
-  private void OnDestroy()
-  {
-    GameHandler.sceneLoader.onNewSceneLoading -= OnNewSceneLoading;
-  }
+    private void OnDestroy()
+    {
+        GameHandler.sceneLoader.onNewSceneLoading -= OnNewSceneLoading;
+    }
 
-  void OnNewSceneLoading(SceneField aScene)
-  {
-    MouseCursor.instance.SetCursor(MouseCursor.instance.defaultCursor);
-    GameHandler.persistencyManager.SetUnlockedState(this);
-  }
+    void OnNewSceneLoading(SceneField aScene)
+    {
+        MouseCursor.instance.SetCursor(MouseCursor.instance.defaultCursor);
+        GameHandler.persistencyManager.SetUnlockedState(this);
+    }
 }
