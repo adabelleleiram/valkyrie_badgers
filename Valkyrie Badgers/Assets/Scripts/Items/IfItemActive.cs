17956c0f90fa661ed35553e91cd5fb87633e86f6
Adabelle Leiram <Adabelle Leiram@PARTYPC>
2018-07-16 19:28:54 +0200

    Spider moves when fly active

---
+++ Valkyrie Badgers/Assets/Scripts/Items/IfItemActive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IfItemActive : MonoBehaviour {
+
+    [System.Serializable]
+    public class ItemActiveEvent : UnityEvent { }
+
+    public GameObject anObject;
+    public ItemActiveEvent onItemActivated;
+
+    bool activated = false;
+
+	void Update () {
+		if(!activated && anObject.activeSelf)
+        {
+            activated = true;
+            onItemActivated.Invoke();
+        }
+	}
+}
