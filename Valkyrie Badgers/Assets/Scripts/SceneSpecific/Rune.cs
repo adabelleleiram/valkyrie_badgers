dfa87a8bdeba94234f6b361334eebfb13d27b688
Adabelle Leiram <Adabelle Leiram@PARTYPC>
2018-07-16 19:13:25 +0200

    Fixed rune puzzle

---
+++ Valkyrie Badgers/Assets/Scripts/SceneSpecific/Rune.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rune : MonoBehaviour {
+
+    public GameObject activeObject;
+    public GameObject highlightObject;
+
+    RunePuzzle puzzle;
+
+
+    private void OnMouseEnter()
+    {
+        if(!activeObject.activeSelf)
+            highlightObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        highlightObject.SetActive(false);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        puzzle.OnRuneClicked(this);
+    }
+
+    public void SetActivated(bool active)
+    {
+        activeObject.SetActive(active);
+    }
+
+    public void SetPuzzle(RunePuzzle aPuzzle)
+    {
+        puzzle = aPuzzle;
+    }
+}
---
+++ Valkyrie Badgers/Assets/Scripts/SceneSpecific/RunePuzzle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RunePuzzle : MonoBehaviour
+{
+    [System.Serializable]
+    public class CompletedEvent : UnityEvent { }
+
+    public List<Rune> runesListInOrder = new List<Rune>();
+    public CompletedEvent onCompleted;
+
+    void Start()
+    {
+        foreach (Rune rune in runesListInOrder)
+        {
+            rune.SetPuzzle(this);
+            rune.SetActivated(false);
+        }
+    }
+
+    public void OnRuneClicked(Rune aRune)
+    {
+        foreach(Rune rune in runesListInOrder)
+        {
+            if(rune == aRune)
+            {
+                rune.SetActivated(true);
+                if (rune == runesListInOrder[runesListInOrder.Count - 1])
+                    onCompleted.Invoke();
+                break;
+            }
+            else if(rune.activeObject.activeSelf == false)
+            {
+                ResetAllRunes();
+                break;
+            }
+        }
+    }
+
+    void ResetAllRunes()
+    {
+        foreach(Rune rune in runesListInOrder)
+        {
+            rune.SetActivated(false);
+        }
+    }
+}
